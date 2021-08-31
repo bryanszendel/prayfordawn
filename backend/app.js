5497169518414638978c583726208324500aaa0d
Bryan Szendel <bryan@bigarmor.com>
2021-08-31 01:47:09 -0400

    code

--- backend/app.js
+++ backend/app.js
@@ -3,7 +3,6 @@ var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 
-var indexRouter = require("./routes/index");
 var timeSlotsRouter = require("./routes/timeSlotsRouter").default;
 
 var app = express();
@@ -14,7 +13,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/", indexRouter);
 app.use("/timeslots", timeSlotsRouter);
 
 module.exports = app;
